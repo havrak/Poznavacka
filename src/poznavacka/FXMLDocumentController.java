11413ffd81b1e13773f610e2b1f03fb46c970fcd
Havra <krystof@havrak.xyz>
2019-11-09 12:19:37 +0100

    Simplified code, added help button

--- src/poznavacka/FXMLDocumentController.java
+++ src/poznavacka/FXMLDocumentController.java
@@ -8,10 +8,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
+import static javafx.scene.input.KeyCode.DOWN;
+import static javafx.scene.input.KeyCode.ENTER;
+import static javafx.scene.input.KeyCode.LEFT;
+import static javafx.scene.input.KeyCode.RIGHT;
+import static javafx.scene.input.KeyCode.UP;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -22,7 +27,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author havra
+ * @author havra, litos
  */
 public class FXMLDocumentController {
 
@@ -63,13 +68,13 @@ public class FXMLDocumentController {
 	@FXML
 	private TextField guessTF;
 
-	private File dir = new File("/home/havra/Documents/School/Second year/Poznávačka");
+	private File dir = new File("/home/havra/Documents/School/Second year/Poznávačka"); // choose your own directory
 	private GraphicsContext gc;
+	private Stage stage;
 	private final ArrayList<File> picList = new ArrayList<>();
 	private final ArrayList<ArrayList<File>> toShow = new ArrayList<>();
 
 	private boolean inGame = false;
-	private Stage stage;
 	private String rightAnswer;
 	private final String[] rightList = new String[6];
 	private int indexOfShown;
@@ -84,57 +89,54 @@ public class FXMLDocumentController {
 		this.stage = stage;
 		gc = Canvas.getGraphicsContext2D();
 		gc.setFont(new Font(15));
-		addFiles(dir);
+		try {
+			addFiles(dir);
+		} catch (NullPointerException e) {
+		}
 		for (int i = 0; i < fields.length; i++) {
 			fields[i] = new TextField();
 			fields[i].setPromptText(prompt(i));
 			fields[i].setMaxSize(200, 30);
 			fields[i].setMinSize(200, 30);
 			fields[i].setOnKeyPressed((ke) -> {
-				switch (ke.getCode()) {
-					case ENTER:
-						if (ke.isShiftDown() || displayingAnswer) {
-							confirmGuessAction(ke);
-							break;
-						}
-					case DOWN:
-						if (ke.isShiftDown()) {
-							move(true);
-						} else {
-							select(ke.getSource(), true);
-						}
-						break;
-					case UP:
-						if (ke.isShiftDown()) {
-							move(false);
-						} else {
-							select(ke.getSource(), false);
-						}
-						break;
-					case LEFT:
-					case RIGHT:
-						if (ke.isShiftDown()) {
-							guessTF.requestFocus();
-						}
-				}
+				switcher(ke, false);
 			});
 		}
 	}
 
-	private void select(Object o, boolean down) {
-		if (list.getChildren().isEmpty()) {
-			return;
+	@FXML
+	private void confirmGuessAction(KeyEvent ke) {
+		if (inGame) {
+			switcher(ke, true);
 		}
-		int index = list.getChildren().indexOf(o);
-		if (down && index == list.getChildren().size() - 1) {
-			list.getChildren().get(0).requestFocus();
-			scrl.setVvalue(0);
-		} else if (index == 0 && !down) {
-			list.getChildren().get(list.getChildren().size() - 1).requestFocus();
-			scrl.setVvalue(scrl.getVmax());
-		} else {
-			list.getChildren().get(index + (down ? 1 : -1)).requestFocus();
-			scrl.setVvalue(index + (down ? 1 : -1));
+	}
+
+	@FXML
+	private void helpAction(ActionEvent event) {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Help");
+		alert.setHeaderText("Help");
+		alert.setWidth(200);
+		alert.setHeight(200);
+		alert.setResizable(true);
+		alert.setContentText("Commiting answer: ENTER\n"
+				+ "Picture:\n"
+				+ "  -Next: SHIFT + ENTER/DOWN or PG_DOWN\n"
+				+ "  -Previous: PAGE_UP or SHIFT + UP\n"
+				+ "Text field:\n"
+				+ "  -Next: CTRL + ENTER or DOWN\n"
+				+ "  -Previous: UP\n"
+				+ "Tgl between 'NR' or list: SHIFT + LEFT/RIGHT");
+		alert.showAndWait();
+	}
+
+	private void addFiles(File src) {
+		for (File f : src.listFiles()) {
+			if (f.isDirectory()) {
+				addFiles(f);
+			} else {
+				picList.add(f);
+			}
 		}
 	}
 
@@ -189,20 +191,10 @@ public class FXMLDocumentController {
 		addFiles(dir);
 	}
 
-	private void addFiles(File src) {
-		for (File f : src.listFiles()) {
-			if (f.isDirectory()) {
-				addFiles(f);
-			} else {
-				picList.add(f);
-			}
-		}
-	}
-
 	@FXML
-	private void btnResetPressed(ActionEvent event) {
+	private void btnStartPressed(ActionEvent event) {
 		if (picList.isEmpty()) {
-			guessTF.setText("Zvolte složku s obrázky.");
+			guessTF.setText("Choose dir with pictures");
 			return;
 		}
 		reset(true);
@@ -228,12 +220,132 @@ public class FXMLDocumentController {
 		setRight();
 	}
 
+	private void reset(boolean b) {
+		if (b) {
+			gc.clearRect(0, 0, Canvas.getWidth(), Canvas.getHeight());
+			guessTF.setText("");
+		}
+		toShow.clear();
+		inGame = false;
+		indexOfShown = 0;
+		displayingAnswer = false;
+		guessedRight = listRight = 0;
+	}
+
 	private String makeName(File f) {
 		String name = f.getName().substring(0, f.getName().lastIndexOf("."));
 		if (Character.isDigit(name.charAt(name.length() - 1))) {
 			name = name.substring(0, name.lastIndexOf(" "));
 		}
 		return name;
+	}
+
+	private void switcher(KeyEvent ke, boolean tf) {
+		switch (ke.getCode()) {
+			case ENTER:
+				if (ke.isControlDown()) {
+					select(ke.getSource(), true);
+				} else if (ke.isShiftDown()) {
+					move(true);
+				} else if (displayingAnswer = !displayingAnswer) {
+					if (guessTF.getText().toLowerCase().trim().equals(rightAnswer)) {
+						gc.setFill(Color.GREEN);
+						gc.fillRect(Canvas.getWidth() / 2 - 110, 20, 220, 40);
+						gc.strokeText("Correct Name", Canvas.getWidth() / 2 - 90, 35);
+						guessTF.setText("");
+						guessedRight++;
+					} else {
+						gc.setFill(Color.RED);
+						gc.fillRect(Canvas.getWidth() / 2 - 110, 20, 220, 40);
+						gc.strokeText("Wrong Name, Right Name:\n" + rightAnswer, Canvas.getWidth() / 2 - 90, 35);
+						guessTF.setText("");
+					}
+					if (testList()) {
+						gc.setFill(Color.GREEN);
+						gc.fillRect(Canvas.getWidth() / 2 - 110, 60, 220, 100);
+						gc.strokeText("Correct List", Canvas.getWidth() / 2 - 90, 75);
+						listRight++;
+					} else {
+						gc.setFill(Color.RED);
+						gc.fillRect(Canvas.getWidth() / 2 - 110, 60, 220, 100);
+						gc.strokeText("Wrong List, Right List:\n" + rightList(), Canvas.getWidth() / 2 - 90, 75);
+						guessTF.setText("");
+					}
+				} else {
+					toShow.remove(0);
+					if (toShow.isEmpty()) {
+						reset(true);
+					} else {
+						setRight();
+					}
+				}
+				break;
+			case DOWN:
+				if (!ke.isShiftDown()) {
+					select(ke.getSource(), true);
+					break;
+				}
+			case PAGE_DOWN:
+				move(true);
+				break;
+			case UP:
+				if (!ke.isShiftDown()) {
+					select(ke.getSource(), false);
+					break;
+				}
+			case PAGE_UP:
+				move(false);
+				break;
+			case LEFT:
+			case RIGHT:
+				if (tf && ke.isShiftDown() && !list.getChildren().isEmpty()) {
+					list.getChildren().get((int) scrl.getVvalue()).requestFocus();
+				} else if (!tf && ke.isShiftDown()) {
+					guessTF.requestFocus();
+				}
+		}
+	}
+
+	@FXML
+	private void btnPrev(ActionEvent event) {
+		move(false);
+	}
+
+	@FXML
+	private void btnNext(ActionEvent event) {
+		move(true);
+	}
+
+	private void select(Object o, boolean down) {
+		if (list.getChildren().isEmpty()) {
+			return;
+		}
+		int index = list.getChildren().indexOf(o);
+		if (index == -1) {
+			list.getChildren().get(down ? 0 : list.getChildren().size() - 1).requestFocus();
+			scrl.setVvalue(down ? 0 : list.getChildren().size() - 1);
+		} else if (down ? index == list.getChildren().size() - 1 : index == 0) {
+			guessTF.requestFocus();
+		} else {
+			list.getChildren().get(index + (down ? 1 : -1)).requestFocus();
+			scrl.setVvalue(index + (down ? 1 : -1));
+		}
+	}
+
+	private void move(boolean next) {
+		if (!displayingAnswer && inGame) {
+			if (next) {
+				if (++indexOfShown == toShow.get(0).size()) {
+					indexOfShown = 0;
+				}
+				drawPic();
+			} else {
+				if (indexOfShown-- == 0) {
+					indexOfShown = toShow.get(0).size() - 1;
+				}
+				drawPic();
+			}
+		}
 	}
 
 	private void setRight() {
@@ -260,78 +372,6 @@ public class FXMLDocumentController {
 		drawPic();
 	}
 
-	private void reset(boolean b) {
-		if (b) {
-			gc.clearRect(0, 0, Canvas.getWidth(), Canvas.getHeight());
-			guessTF.setText("");
-		}
-		toShow.clear();
-		inGame = false;
-		indexOfShown = 0;
-		displayingAnswer = false;
-		guessedRight = listRight = 0;
-	}
-
-	@FXML
-	private void confirmGuessAction(KeyEvent event) {
-		if (inGame) {
-			KeyCode kc = event.getCode();
-			switch (kc) {
-				case ENTER:
-					if (!displayingAnswer) {
-						displayingAnswer = true;
-						System.out.println(guessTF.getText().toLowerCase().trim()+"+"+rightAnswer);	
-						if (guessTF.getText().toLowerCase().trim().equals(rightAnswer)) {
-							gc.setFill(Color.GREEN);
-							gc.fillRect(Canvas.getWidth() / 2 - 110, 20, 220, 40);
-							gc.strokeText("Correct", Canvas.getWidth() / 2 - 90, 35);
-							guessTF.setText("");
-							guessedRight++;
-						} else {
-							gc.setFill(Color.RED);
-							gc.fillRect(Canvas.getWidth() / 2 - 110, 20, 220, 40);
-							gc.strokeText("Wrong, Right Answer:\n" + rightAnswer, Canvas.getWidth() / 2 - 90, 35);
-							guessTF.setText("");
-						}
-						if (testList()) {
-							gc.setFill(Color.GREEN);
-							gc.fillRect(Canvas.getWidth() / 2 - 110, 60, 220, 100);
-							gc.strokeText("Correct", Canvas.getWidth() / 2 - 90, 75);
-							listRight++;
-						} else {
-							gc.setFill(Color.RED);
-							gc.fillRect(Canvas.getWidth() / 2 - 110, 60, 220, 100);
-							gc.strokeText("Wrong, Right Answer:\n" + rightList(), Canvas.getWidth() / 2 - 90, 75);
-							guessTF.setText("");
-						}
-					} else {
-						toShow.remove(0);
-						displayingAnswer = false;
-						if (toShow.isEmpty()) {
-							reset(true);
-						} else {
-							setRight();
-						}
-					}
-					break;
-				case PAGE_DOWN:
-				case DOWN:
-					move(true);
-					break;
-				case PAGE_UP:
-				case UP:
-					move(false);
-					break;
-				case RIGHT:
-				case LEFT:
-					if (event.isShiftDown() && !list.getChildren().isEmpty()) {
-						list.getChildren().get(0).requestFocus();
-						scrl.setVvalue(0);
-					}
-			}
-		}
-	}
-
 	private String rightList() {
 		String s = "";
 		for (int i = 0; i < list.getChildren().size(); i++) {
@@ -344,7 +384,6 @@ public class FXMLDocumentController {
 		for (int i = 0; i < list.getChildren().size(); i++) {
 			boolean match = false;
 			for (String s : rightList[i].toLowerCase().split(",")) {
-				System.out.println(s);
 				if (match = ((TextField) list.getChildren().get(i)).getText().toLowerCase().equals(s)) {
 					break;
 				}
@@ -356,36 +395,9 @@ public class FXMLDocumentController {
 		return true;
 	}
 
-	@FXML
-	private void btnPrev(ActionEvent event) {
-		move(false);
-	}
-
-	@FXML
-	private void btnNext(ActionEvent event) {
-		move(true);
-	}
-
-	private void move(boolean next) {
-		if (!displayingAnswer && inGame) {
-			if (next) {
-				if (++indexOfShown == toShow.get(0).size()) {
-					indexOfShown = 0;
-				}
-				drawPic();
-			} else {
-				if (indexOfShown-- == 0) {
-					indexOfShown = toShow.get(0).size() - 1;
-				}
-				drawPic();
-			}
-		}
-	}
-
 	private void drawPic() {
 		File file = toShow.get(0).get(indexOfShown);
-		if (file.getName().toLowerCase().endsWith(".png") || file.getName().toLowerCase().endsWith(".jpg")
-				|| file.getName().toLowerCase().endsWith(".jpeg")) {
+		if (file.getName().toLowerCase().endsWith(".png") || file.getName().toLowerCase().endsWith(".jpg") || file.getName().toLowerCase().endsWith(".jpeg") || file.getName().toLowerCase().endsWith(".gif")) { // not totaly sure about gif
 			Image image = new Image(file.toURI().toString());
 			double hRatio = Canvas.getWidth() / image.getWidth();
 			double vRatio = Canvas.getHeight() / image.getHeight();
